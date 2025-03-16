4f80018954b2988ae5c395ac526c6751847a1390
lzh desktop <2232284193@qq.com>
2025-03-16 19:31:00 +0800

    update v1 code and test code

--- v1/include/MemoryPool.h
+++ v1/include/MemoryPool.h
@@ -31,7 +31,7 @@ namespace Memory_Pool
         // 分配内存
         void *allocate();
         // 释放内存
-        void deallocate(void *);
+        void deallocate(void *ptr);
 
     private:
         // 分配新的内存块
@@ -43,20 +43,23 @@ namespace Memory_Pool
         Slot *popFreeList();
 
     private:
-        int BlockSize_;                // 内存块大小
-        int SlotSize_;                 // 槽大小
-        Slot *firstBlock_;             // 指向内存池管理的首个实际内存块
-        Slot *curSlot_;                // 指向当前未被使用过的槽
-        Slot *freeLists_;              // 指向空闲的槽(被使用过又被释放的槽)
-        Slot *lastSlot_;               // 指向空闲的槽
-        std::mutex mutexForFreeLists_; // 保证freeList_在多线程中操作的原子性
-        std::mutex mutexForBlock_;     // 保证多线程情况下避免不必要的重复开辟内存导致的浪费行为
+        int BlockSize_;    // 内存块大小
+        int SlotSize_;     // 槽大小
+        Slot *firstBlock_; // 指向内存池管理的首个实际内存块
+        Slot *curSlot_;    // 指向当前未被使用过的槽
+        // Slot *freeLists_;              // 指向空闲的槽(被使用过又被释放的槽)
+        std::atomic<Slot *> freeLists_; // 指向空闲的槽(被使用过又被释放的槽)
+        Slot *lastSlot_;                // 指向当前内存块中最后能存放元素的位置标识
+        // std::mutex mutexForFreeLists_;  // 保证freeList_在多线程中操作的原子性
+        std::mutex mutexForBlock_; // 保证多线程情况下避免不必要的重复开辟内存导致的浪费行为
     };
 
     class HashBucket
     {
     public:
+        // 初始化内存池
         static void initMemoryPool();
+        // 单例模式
         static MemoryPool &getMemoryPool(int index);
 
         // 分配内存
@@ -72,4 +75,29 @@ namespace Memory_Pool
         friend void deleteElement(T *p);
     };
 
-}
+    template <typename T, typename... Args>
+    T *newElement(Args &&...args)
+    {
+        T *p = nullptr;
+        // 根据元素大小选取合适的内存分配池分配内存
+        if ((p = reinterpret_cast<T *>(HashBucket::useMemory(sizeof(T)))) != nullptr)
+        {
+            // 在分配的内存上构造对象
+            new (p) T(std::forward<Args>(args)...);
+        }
+
+        return p;
+    }
+
+    template <typename T, typename... Args>
+    void deleteElement(T *p)
+    {
+        // 对象析构
+        if (p)
+        {
+            p->~T();
+            // 内存回收
+            HashBucket::freeMemory(reinterpret_cast<void *>(p), sizeof(T));
+        }
+    }
+} // namespace Memory_Pool
