16baeb57306f0a7221bd61a145ca510e29cf71ae
lzh desktop <2232284193@qq.com>
2025-03-23 20:31:51 +0800

    添加页缓存的定义

--- v2/include/PageCache.h
+++ v2/include/PageCache.h
@@ -41,7 +41,8 @@ namespace Memory_Pool
         // 按页数管理空闲span，不同页数对应不同Span链表
         std::map<size_t, Span *> freeSpans_;
 
-        // 页号到span的映射，用于回收
+        // 页号到span的映射，记录所有已分配的span，即所有span，用于回收
+        // !!这里的已分配指的是所有正在使用的+已经释放的（在freeSpans_中记录）
         std::map<void *, Span *> spanMap_;
 
         std::mutex mutex_;
